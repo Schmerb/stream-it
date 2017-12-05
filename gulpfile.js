8f5fea7f873f65115f5503714d5d1fd629fc5ff8
Schmerb <mike.schmerbeck@gmail.com>
2017-12-05 14:42:56 -0500

    minify css in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@
 const gulp        = require('gulp'),
       babel       = require('gulp-babel'),
       browserify  = require('gulp-browserify'),
-      minify      = require('gulp-minify'),
+	  minify      = require('gulp-minify'),
+	  minifyCSS   = require('gulp-clean-css'),
+	  rename      = require('gulp-rename'),
       concat      = require('gulp-concat');
       
 
@@ -41,4 +43,12 @@ gulp.task('watch_es6', () => {
 	gulp.watch(JS_SRC, ['build_es6']);
 })
 
-gulp.task('default', ['build_es6', 'watch_es6']);
+const CSS_SRC = 'src/styles/main.css';
+gulp.task('minify_css', () => {
+	return gulp.src(CSS_SRC)
+		.pipe(minifyCSS())
+		.pipe(rename({suffix: '-min'}))
+		.pipe(gulp.dest('build'))
+});
+
+gulp.task('default', ['build_es6', 'watch_es6', 'minify_css']);
